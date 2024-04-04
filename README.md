1d772147845710be131a313f760be626e65864ce
mggggk <mggggk@gmail.com>
2024-04-04 23:13:28 +0900

    Add scripts

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # discord-bot-sample
 Discord bot sample.
+
+## Install
+
+- [Setting up a bot application | discord.js Guide](https://discordjs.guide/preparations/setting-up-a-bot-application.html#creating-your-bot)
+
+## Usage
+
+```bash
+cp src/config-default.json src/config.json
+
+# Edit it
+vi src/config.json
+```
+
+# Memo
+
+## Debug
+
+```js
+client.on('debug', console.log);
+```
+
+```yml
+
+      - ./bot/:/bot/
+
+    command: "tail  \
+          -f \
+          /dev/null"
+```
+
+- [Optimization and Troubleshooting | Discord.js Guide](https://v12.discordjs.guide/voice/optimisation-and-troubleshooting.html#using-ogg-webm-opus-streams)
---
+++ src/index.js
@@ -0,0 +1,95 @@
+const { Client, Events, GatewayIntentBits, REST, Routes } = require('discord.js');
+const { clientId, guildId, token } = require('./config.json');
+const fs = require('node:fs');
+const path = require('node:path');
+
+const commands = [];
+// Grab all the command files from the commands directory you created earlier
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	// Grab all the command files from the commands directory you created earlier
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		if ('data' in command && 'execute' in command) {
+			commands.push(command.data.toJSON());
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
+
+// Construct and prepare an instance of the REST module
+const rest = new REST().setToken(token);
+
+// and deploy your commands!
+(async () => {
+	try {
+		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+		// The put method is used to fully refresh all commands in the guild with the current set
+		const data = await rest.put(
+			Routes.applicationGuildCommands(clientId, guildId),
+			{ body: commands },
+		);
+
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} catch (error) {
+		// And of course, make sure you catch and log any errors!
+		console.error(error);
+	}
+})();
+
+
+
+const heyFile = require('./commands/hey/index.js');
+
+// クライアントインスタンスと呼ばれるオブジェクトを作成します
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// クライアントオブジェクトが準備OKとなったとき一度だけ実行されます
+client.once(Events.ClientReady, c => {
+	console.log(`準備OKです! ${c.user.tag}がログインします。`);
+  c.application.commands.set([])
+  console.log(`Remove all app slash command`);
+
+  const guild = client.guilds.cache.get(guildId);
+  guild.commands.set([])
+    .then(console.log)
+    .catch(console.error);
+  console.log(`Remove all guild slash command`);
+});
+
+//スラッシュコマンドに応答するには、interactionCreateのイベントリスナーを使う必要があります
+client.on(Events.InteractionCreate, async interaction => {
+
+    // スラッシュ以外のコマンドの場合は対象外なので早期リターンさせて終了します
+    // コマンドにスラッシュが使われているかどうかはisChatInputCommand()で判断しています
+    if (!interaction.isChatInputCommand()) return;
+
+    // heyコマンドに対する処理
+    if (interaction.commandName === heyFile.data.name) {
+        try {
+            await heyFile.execute(interaction);
+        } catch (error) {
+            console.error(error);
+            if (interaction.replied || interaction.deferred) {
+                await interaction.followUp({ content: 'コマンド実行時にエラーになりました。', ephemeral: true });
+            } else {
+                await interaction.reply({ content: 'コマンド実行時にエラーになりました。', ephemeral: true });
+            }
+        }
+    } else {
+        console.error(`${interaction.commandName}というコマンドには対応していません。`);
+    }
+});
+
+
+// ログインします
+client.login(token);
